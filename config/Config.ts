658f0a254551cddd0f47801ae9cec444d0e41b06
Dhrubajyoti Mukherjee <117375130+marjet26@users.noreply.github.com>
2024-08-27 12:32:26 +0200

    feat: update config

--- config/Config.ts
+++ config/Config.ts
@@ -29,13 +29,13 @@ export const DZ_DOMAIN_DESCRIPTION = 'DataZone domain for CORP';
 export const DZ_DOMAIN_TAG = 'CorpDev';
 export const DZ_ADMIN_PROJECT_NAME = 'Admin';
 export const DZ_ADMIN_PROJECT_DESCRIPTION = 'Project for Data Solution Administrators';
-export const CDK_EXEC_ROLE_ARN = 'arn:aws:iam::111222333444:role/cdk-xyzabcd-cfn-exec-role-111222333444-us-east-1';
+export const CDK_EXEC_ROLE_ARN = 'arn:aws:iam::111222333444:role/cdk-hnb659fds-cfn-exec-role-111222333444-us-east-1';
 export const DZ_ADMIN_ROLE_ARN = 'arn:aws:iam::111222333444:role/Admin';
 export const DZ_IAM_USER_ID_LIST   = ['TEST123'];  //allcaps
 export const DZ_MEMBER_ACCOUNT_LIST  = [];
 export const DZ_MEMBER_STACK_SET_EXEC_ROLE_LIST  = [''];
-export const DZ_ADMINISTRATOR_EMAIL = 'user+dzadmin@example.com';
-export const DZ_COST_NOTIFICATION_EMAIL = 'user+dzcost@example.com';
+export const DZ_ADMINISTRATOR_EMAIL = 'user+dzadmin@acmecorp.domain';
+export const DZ_COST_NOTIFICATION_EMAIL = 'user+dzcost@acmecorp.domain';
 
 
 // Keep blank if you don't have member accounts
@@ -49,6 +49,6 @@ export const DZ_MEMBER_ACCOUNT_CONFIG: memberAccountConfig = {
   '123456789012' : {
     PROJECT_NAME: 'TEST-PROJECT-123456789012',
     PROJECT_DESCRIPTION: 'TEST-PROJECT-123456789012',
-    PROJECT_EMAIL: 'dimukher+project@amazon.de'
+    PROJECT_EMAIL: 'memeber+project@acmecorp.domain'
   }
 }*/
